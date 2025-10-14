a6536c5dd71d2169a943d095dc63c46164a9a673
JC <creeperdwag111@gmail.com>
2025-10-13 21:25:16 -0700

    fix: resolve eslint no-inner-declarations warn

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,11 @@
-<h1>welcome to the frontend</h1>
-<p>so cool</p>
-
-
 <script>
-    async function testAPI() {
-      const response = await fetch('http://localhost:5001/api/hello');
-      const data = await response.json();
-      alert(JSON.stringify(data, null, 2));
-    }
-  </script>
-  
-  <h1>Canvas and Clay</h1>
-  <button on:click={testAPI}>Test API</button>
+  const testAPI = async () => {
+    const response = await fetch('http://localhost:5001/api/hello');
+    const data = await response.json();
+    alert(JSON.stringify(data, null, 2));
+  };
+</script>
+
+<h1>Canvas and Clay</h1>
+<p>Welcome to the frontend</p>
+<button on:click={testAPI}>Test API</button>
